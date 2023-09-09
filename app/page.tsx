99c777c8567e3c6d5ad8ce4b31059342f7d34ba1
khairul alam <khairul.alam@vivacomsolutions.com>
2023-09-09 22:48:24 +0600

    initial

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+//import Image from 'next/image'
+//import styles from './page.module.css'
+import { Button } from "antd";
+
+export default function Home() {
+  console.log("Home Page");
+  return (
+    <div>
+      <Button type="primary">test</Button>
+    </div>
+  );
+}
